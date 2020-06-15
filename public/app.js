34b46d76794d2705fe9120387bb28417be801570
James Gale <jgaleiphone@gmail.com>
2020-06-15 00:22:24 -0400

    Fixed a bug

--- public/app.js
+++ public/app.js
@@ -62,8 +62,7 @@ let side = $(".large");
 
 let ideas;
 
-// $(".input-main").hide();
-
+// JQuery items
 $("#start-search").on("click", async function (event) {
   event.preventDefault();
   mealHide();
@@ -71,6 +70,145 @@ $("#start-search").on("click", async function (event) {
   searchTerm = $(".search-terms").val();
   getSearch();
 });
+
+$("#load-defaults").on("click", function (event) {
+  event.preventDefault();
+  localStorage.clear();
+  meal = defaultz;
+  localStorage.setItem("meals", JSON.stringify(meal));
+  alert("Defaults Now Loaded");
+});
+
+$("#canceldelete").on("click", function () {
+  main.empty();
+  mealHide();
+});
+
+$(".clearall").on("click", function (event) {
+  event.preventDefault();
+  mealHide();
+  let r = confirm(
+    "Press ok to clear everthing, or cancel to keep current settings"
+  );
+
+  if (r === true) {
+    txt = "Clearing Everything";
+    localStorage.clear();
+    location.reload();
+  } else {
+    txt = "Cancel";
+  }
+});
+
+$("#display-meals").on("click", function (event) {
+  event.preventDefault();
+  mealShow();
+  main.empty();
+  meal.forEach(Function1);
+});
+
+$("#messagesend").on("click", function (event) {
+  event.preventDefault();
+  let newMeal = $("#input-message").val();
+  meal.push(newMeal);
+  $("#input-message").val(" ");
+  storeData();
+  shuffle(meal);
+  mealHide();
+});
+
+$("#random").on("click", function (event) {
+  event.preventDefault();
+  mealHide();
+  retrieveData();
+  x = meal;
+  main.empty();
+  shuffle(x);
+  if (`${x[0]}` === "undefined") {
+    main.append(
+      `<div class="display">No meals entered, please enter a meal or load default meals</div>`
+    );
+  } else {
+    main.append(
+      `<div class="display"><div id="d1">
+           Dinner Day 1 = ${x[0]} <br></div>
+     <div id="d2">Dinner Day 2 = ${x[1]}<br></div>
+     <div id="d3">Dinner Day 3 = ${x[2]}<br></div>
+     <div id="d4">Dinner Day 4 = ${x[3]}<br></div>
+     <div id="d5">Dinner Day 5 = ${x[4]}<br></div>
+     <div id="d6">Dinner Day 6 = ${x[5]}<br></div>
+     <div id="d7">Dinner Day 7 = ${x[6]}</div></div> `
+    );
+  }
+});
+
+$("#random-api-meals").on("click", function (event) {
+  event.preventDefault();
+  randomApiMeals();
+});
+
+$("#searchmeal").on("click", function () {
+  console.log("search clicked");
+  searchTerm = $(".messageCheckbox:checked")
+    .closest("#meals")
+    .find("#meal-name")
+    .text();
+  getSearch();
+});
+
+//Blues - document ready etc
+
+span.onclick = function () {
+  $(".wine-clear").text(" ");
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    $(".wine-clear").text(" ");
+    modal.style.display = "none";
+  }
+};
+
+btn.onclick = function () {
+  console.log("clicked");
+  modal.style.display = "block";
+};
+
+$(document).ready(function () {
+  $("#deletemeal").on("click", function () {
+    if (confirm("Are you sure to delete selected meal?")) {
+      $(":checkbox").each(function () {
+        let that = $(this);
+        if (that.is(":checked")) {
+          let obj = $(this).closest("#meals").find("#hide").text();
+          let idx = obj;
+          meal.splice(idx, 1);
+          storeData();
+          main.empty();
+          meal.forEach(Function1);
+          console.log("ran");
+        }
+      });
+    }
+  });
+});
+
+$(document).on("click", ".add-meal", function () {
+  console.log("clicky");
+  let newMeal = $(this).closest(".mealsearch").find(".add-meal-click");
+  let mealAdd = newMeal.text();
+  console.log(mealAdd);
+  meal.push(mealAdd);
+  storeData();
+  console.log("complete");
+});
+
+$(document).on("click", "#view-wine", function () {
+  getWine();
+});
+
+//Functions
 
 async function foodTrivia() {
   try {
@@ -115,10 +253,6 @@ function displayWine(winez) {
   }
 }
 
-$(document).on("click", "#view-wine", function () {
-  getWine();
-});
-
 async function getSearch() {
   try {
     let response = await fetch(`/search?searchTerm=${searchTerm}`),
@@ -132,7 +266,6 @@ async function getSearch() {
   } catch (error) {
     console.log(error);
   }
-  // $(".search-terms").val("");
 }
 
 async function randomApiMeals() {
@@ -160,26 +293,6 @@ function displayData(post) {
   <h4>Ready in: ${post.readyInMinutes} minutes <br> Servings: ${post.servings} </h4> <br> <br><img src="https://spoonacular.com/recipeImages/${post.image}" height="250" width="250"><br> <br><button id="view-wine">View Wine Pairing</button><br><br><button class="add-meal">Add Meal</button></div>`);
 }
 
-$("#load-defaults").on("click", function (event) {
-  event.preventDefault();
-  localStorage.clear();
-  meal = defaultz;
-  localStorage.setItem("meals", JSON.stringify(meal));
-  alert("Defaults Now Loaded");
-});
-
-$(document).on("click", ".add-meal", function () {
-  console.log("clicky");
-  let newMeal = $(this).closest(".mealsearch").find(".add-meal-click");
-  let mealAdd = newMeal.text();
-
-  console.log(mealAdd);
-  meal.push(mealAdd);
-  meals.push(mealAdd);
-  storeData();
-  console.log("complete");
-});
-
 function shuffle(array) {
   mealHide();
   let currentIndex = meal.length,
@@ -197,34 +310,6 @@ function shuffle(array) {
   return array;
 }
 
-$("#canceldelete").on("click", function () {
-  main.empty();
-  mealHide();
-});
-
-$(".clearall").on("click", function (event) {
-  event.preventDefault();
-  mealHide();
-  let r = confirm(
-    "Press ok to clear everthing, or cancel to keep current settings"
-  );
-
-  if (r === true) {
-    txt = "Clearing Everything";
-    localStorage.clear();
-    location.reload();
-  } else {
-    txt = "Cancel";
-  }
-});
-
-$("#display-meals").on("click", function (event) {
-  event.preventDefault();
-  mealShow();
-  main.empty();
-  meal.forEach(Function1);
-});
-
 function Function1(currentValue, index) {
   main.append(
     `<div id="meals"><input type="checkbox" class="messageCheckbox">${
@@ -232,68 +317,6 @@ function Function1(currentValue, index) {
     }. <span id="meal-name">${currentValue}</span><div id="hide">${index}</div></div>`
   );
 }
-
-$("#searchmeal").on("click", function () {
-  console.log("search clicked");
-  searchTerm = $(".messageCheckbox:checked")
-    .closest("#meals")
-    .find("#meal-name")
-    .text();
-  getSearch();
-});
-
-$(document).ready(function () {
-  $("#deletemeal").on("click", function () {
-    if (confirm("Are you sure to delete selected meal?")) {
-      $(":checkbox").each(function () {
-        let that = $(this);
-        if (that.is(":checked")) {
-          let obj = $(this).closest("#meals").find("#hide").text();
-          let idx = obj;
-          meal.splice(idx, 1);
-          main.empty();
-          meal.forEach(Function1);
-          console.log("ran");
-        }
-      });
-    }
-  });
-});
-
-$("#messagesend").on("click", function (event) {
-  event.preventDefault();
-  let newMeal = $("#input-message").val();
-  meal.push(newMeal);
-  $("#input-message").val(" ");
-  storeData();
-  shuffle(meal);
-  mealHide();
-});
-
-$("#random").on("click", function (event) {
-  event.preventDefault();
-  mealHide();
-  retrieveData();
-  x = meal;
-  main.empty();
-  shuffle(x);
-  if (`${x[0]}` === "undefined") {
-    main.append(
-      `<div class="display">No meals entered, please enter a meal or load default meals</div>`
-    );
-  } else {
-    main.append(
-      `<div class="display"><div id="d1">
-           Dinner Day 1 = ${x[0]} <br></div>
-     <div id="d2">Dinner Day 2 = ${x[1]}<br></div>
-     <div id="d3">Dinner Day 3 = ${x[2]}<br></div>
-     <div id="d4">Dinner Day 4 = ${x[3]}<br></div>
-     <div id="d5">Dinner Day 5 = ${x[4]}<br></div>
-     <div id="d6">Dinner Day 6 = ${x[5]}<br></div>
-     <div id="d7">Dinner Day 7 = ${x[6]}</div></div> `
-    );
-  }
-});
 
 function mealShow() {
   $("#deletemeal").show();
@@ -307,11 +330,16 @@ function mealHide() {
   $("#searchmeal").hide();
 }
 function storeData() {
+  console.log("Storing data");
   localStorage.setItem("meals", JSON.stringify(meal));
 }
 
 function retrieveData() {
   meal = JSON.parse(localStorage.getItem("meals"));
+}
+
+function modalOpen() {
+  modal.style.display = "block";
 }
 
 function bootStrap() {
@@ -329,31 +357,5 @@ function bootStrap() {
   shuffle(meal);
   storeData();
 }
-
-btn.onclick = function () {
-  console.log("clicked");
-  modal.style.display = "block";
-};
-
-function modalOpen() {
-  modal.style.display = "block";
-}
-
-$("#random-api-meals").on("click", function (event) {
-  event.preventDefault();
-  randomApiMeals();
-});
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  $(".wine-clear").text(" ");
-  modal.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    $(".wine-clear").text(" ");
-    modal.style.display = "none";
-  }
-};
 
 bootStrap();
